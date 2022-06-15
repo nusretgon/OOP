ec087a3e8109e9814319b96623d2bd14a201b65c
Nusret GON <72438433+nusretgon@users.noreply.github.com>
2022-06-15 15:00:10 +0300

    Add files via upload

---
+++ C++/classDeclaration.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+class myclass{
+	public:
+		int a;
+};
+int main(int argc, char** argv) {
+	myclass obj1;
+	cout<<obj1.a<<endl;
+	return 0;
+}
